b6614dba4810bde7f1e53fad7ebfe48f9374a1f3
Alexander Pastoriza <apast005@fiu.edu>
2020-04-08 09:43:50 -0400

    Test Commit

---
+++ mongo-java-driver/src/main/java/org/mongodb/App.java
@@ -0,0 +1,24 @@
+package org.mongodb;
+
+/**
+ * 
+ * Project 3:
+ * - Docker with MongoDB Image
+ * - Develop two groups of three containers
+ * to preform a desired task (ie. Database Actions)
+ * - Use a visualizer tool to graphically display
+ * Network Topology
+ * 
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Unconfirmned what it will do
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
